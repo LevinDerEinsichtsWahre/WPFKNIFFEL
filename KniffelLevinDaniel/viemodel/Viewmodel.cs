c0fb1b839fa57c6ddab0d63b2c22bb95d4723ad5
DaniuelFink <Daniel_Fink2001@gmx.de>
2025-01-24 13:37:41 +0100

    Update Viewmodel.cs
    REQ: Add initial Viewmodel

--- KniffelLevinDaniel/viemodel/Viewmodel.cs
+++ KniffelLevinDaniel/viemodel/Viewmodel.cs
@@ -6,7 +6,68 @@ using System.Threading.Tasks;
 
 namespace KniffelLevinDaniel.viemodel
 {
-    internal class Viewmodel
+    class Viewmodel : INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ICommand NeuWürfeln { get; private set; }
+
+        public List<Dice> Dices { get; private set; }
+
+        public string Dice1Text
+        {
+            get
+            {
+                return "1"
+            }
+
+            private set;
+        }
+
+        public string Dice2Text
+        {
+            get
+            {
+                return "1"
+            }
+
+            private set;
+        }
+
+        public string Dice3Text
+        {
+            get
+            {
+                return "1"
+            }
+
+            private set;
+        }
+
+        public string Dice4Text
+        {
+            get
+            {
+                return "1"
+                }
+
+            private set;
+        }
+
+        public string Dice5Text
+        {
+            get
+            {
+                return "1"
+                }
+
+            private set;
+        }
+
+        public MainViewModel()
+        {
+            this.NeuWürfeln = new NeuWürfeln(this);
+        }
+
     }
 }
