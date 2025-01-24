7fd32e72996a3cef215e82badcea3570b4e0d729
DaniuelFink <Daniel_Fink2001@gmx.de>
2025-01-24 13:51:41 +0100

    Update Score.cs
    REQ: Add Score class

--- KniffelLevinDaniel/model/Score.cs
+++ KniffelLevinDaniel/model/Score.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace KniffelLevinDaniel.model
 {
-    internal class Score
+    public class Score
     {
+
+        public int One { get; set; }
+        public int Two { get; set; }
+        public int Three { get; set; }
+        public int Four { get; set; }
+        public int Five { get; set; }
+        public int Six { get; set; }
+        public int Bonus { get; set; }
+        public int ThreeOfAKind { get; set; }
+        public int FourOfAKind { get; set; }
+        public int FullHouse { get; set; }
+        public int SmallStreet { get; set; }
+        public int BigStreet { get; set; }
+        public int Kniffel { get; set; }
+        public int Chance { get; set; }
+        
     }
 }
