670636cb589520259d20ce6c2c4fb8704e89e91c
Levin Erlacher <lerlacher@artiso.com>
2025-01-24 13:36:52 +0100

    added game model

---
+++ KniffelLevinDaniel/model/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KniffelLevinDaniel.model
+{
+    internal class Game
+    {
+        public List<Player> Players { get; set; }
+
+        public int TurnNumber { get; set; }
+
+        public Game(List<Player> players)
+        {
+            Players = players;
+            TurnNumber = 0;
+        }
+    }
+}
