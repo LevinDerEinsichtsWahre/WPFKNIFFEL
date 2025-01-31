6f390fd3aa760d171ca8ee29b4c347ae2f6eec8d
Daniel Fink <daniel.fink@elektrobit.com>
2025-01-31 09:07:34 +0100

    REQ: Add score logic to count the score

--- KniffelLevinDaniel/model/Score.cs
+++ KniffelLevinDaniel/model/Score.cs
@@ -8,20 +8,179 @@ namespace KniffelLevinDaniel.model
 {
     public class Score
     {
-        public int One { get; set; }
-        public int Two { get; set; }
-        public int Three { get; set; }
-        public int Four { get; set; }
-        public int Five { get; set; }
-        public int Six { get; set; }
-        public int Bonus { get; set; }
-        public int ThreeOfAKind { get; set; }
-        public int FourOfAKind { get; set; }
-        public int FullHouse { get; set; }
-        public int SmallStreet { get; set; }
-        public int BigStreet { get; set; }
-        public int Kniffel { get; set; }
-        public int Chance { get; set; }
+        public int One 
+        { 
+            get
+            {
+                return One;
+            }
+            set 
+            {
+                One = value;
+            } 
+        }
+        public int Two 
+        {
+            get
+            {
+                return Two;
+            }
+            set
+            {
+                Two = value;
+            }
+        }
+        public int Three 
+        {
+            get
+            {
+                return Three;
+            }
+            set
+            {
+                Three = value;
+            }
+        }
+        public int Four 
+        {
+            get
+            {
+                return Four;
+            }
+            set
+            {
+                Four = value;
+            }
+        }
+        public int Five 
+        {
+            get
+            {
+                return Five;
+            }
+            set
+            {
+                Five = value;
+            }
+        }
+        public int Six 
+        {
+            get
+            {
+                return Six;
+            }
+            set
+            {
+                Six = value;
+            }
+        }
+        public int Bonus 
+        {
+            get
+            {
+                int bonus = 0;
+                if (getCountTop() > 62)
+                {
+                    bonus = 35;
+                }
+                return bonus;
+            }
+            private set {} 
+        }
+        public int ThreeOfAKind 
+        {
+            get
+            {
+                return ThreeOfAKind;
+            }
+            set
+            {
+                ThreeOfAKind = value;
+            }
+        }
+        public int FourOfAKind 
+        {
+            get
+            {
+                return FourOfAKind;
+            }
+            set
+            {
+                FourOfAKind = value;
+            }
+        }
+        public int FullHouse 
+        {
+            get
+            {
+                return FullHouse;
+            }
+            set
+            {
+                FullHouse = value;
+            }
+        }
+        public int SmallStreet 
+        {
+            get
+            {
+                return SmallStreet;
+            }
+            set
+            {
+                SmallStreet = value;
+            }
+        }
+        public int BigStreet 
+        {
+            get
+            {
+                return BigStreet;
+            }
+            set
+            {
+                BigStreet = value;
+            }
+        }
+        public int Kniffel 
+        {
+            get
+            {
+                return Kniffel;
+            }
+            set
+            {
+                Kniffel = value;
+            }
+        }
+        public int Chance 
+        {
+            get
+            {
+                return Chance;
+            }
+            set
+            {
+                Chance = value;
+            }
+        }
+
+        public int getCountTop()
+        {
+            return One + Two + Three + Four + Five + Six;
+        }
+
+        public int getCountDown()
+        {
+            return ThreeOfAKind + FourOfAKind + FullHouse + SmallStreet + BigStreet + Kniffel + Chance;
+        }
+
+        public int getCountAll()
+        {
+            return getCountTop() + getCountDown();
+        }
+
+
         
     }
 }
